b00f17b979c78f06dbbcc410e7f0f44f2e71b688
Miraaa <iantsamirantsoa@gmail.com>
2023-05-08 17:43:15 +0300

    Update GzipMiddleware.php

--- app/Http/Middleware/GzipMiddleware.php
+++ app/Http/Middleware/GzipMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class GzipMiddleware
 {
@@ -14,7 +15,7 @@ class GzipMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
         $response->header('Content-Encoding', 'gzip');
